797d65bf23a592c4ac04b774a83c9af51d0dc7d8
Daniel <dherzog100@gmail.com>
2022-06-16 23:18:58 -0700

    added text content for data rendered

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var apiKey = "051fc8dd93f8f02bd1aa6c78e2aaff5e";
 var url = (cityName) =>
-  `http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`;
+  `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiKey}`;
+
+  var currentEL = document.querySelector("#city-card")
 
 $(document).ready(function () {
   $("#submit-btn").click(function () {
@@ -8,13 +10,26 @@ $(document).ready(function () {
     fetch(url(city))
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
-        dataGrabber(data);
+    getWeather(data[0])
       });
   });
-});
+}); 
 
-function dataGrabber(dataObject) {
+function getWeather(data) {
+  var lat = data.lat 
+  var long = data.lon
+  var city = data.name
+  var url = `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&units=imperial&exclude=minutely,hourly&appid=${apiKey}`;
+fetch(url)
+.then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+currentWeather(city,data)
+    
+      })
+}
+
+function dataGrabber(city,dataObject) {
   // Filtering through the list to get one entry for each day
   var forecast = [];
   var today = new Date();
@@ -22,8 +37,8 @@ function dataGrabber(dataObject) {
     var year = today.getFullYear();
     var month = today.getMonth();
     var date = today.getDate();
-    for (var listi = 0; listi < dataObject.list.length; listi++) {
-      var entry = dataObject.list[listi];
+    for (var listi = 0; listi < dataObject.daily.length-4; listi++) {
+      var entry = dataObject.daily[listi];
       var entryDate = new Date(entry.dt * 1000);
       if (
         entryDate.getFullYear() === year &&
@@ -34,7 +49,38 @@ function dataGrabber(dataObject) {
         break;
       }
     }
-    today.setDate(today.getDate() + 1);
+    today.setDate(today.getDate());
   }
   console.log(forecast)
 }
+
+function currentWeather(city,dataObject) {
+var tempEl = document.createElement("p")
+var windEl = document.createElement("p")
+var humidityEl = document.createElement("p")
+var uviEl = document.createElement("p")
+tempEl.textContent = dataObject.current.temp
+windEl.textContent = dataObject.current.wind_speed
+humidityEl.textContent = dataObject.current.humidity
+uviEl.textContent = dataObject.current.uvi 
+currentEL.append(tempEl, windEl,humidityEl,uviEl)
+futureWeather(city, dataObject) 
+}
+
+function futureWeather(city,dataObject) {
+var day2Temp = document.createElement("p")
+var day2windEl = document.createElement("p")
+var day2humidity = document.createElement("p")
+var uviEl = document
+
+
+ar day3Temp =document.createElement("p")
+
+day2Temp.textContent = dataObject.daily[0].temp
+
+}
+
+
+
+
+
